ce7d48ee988fb31f537b168c9658b8b7ab9bc75d
YaeZed <1511623546@qq.com>
2026-06-12 10:20:21 +0800

    Add mobile dev environment

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+if (import.meta.env.DEV && import.meta.env.VITE_ENABLE_VCONSOLE === "true") {
+  import("vconsole").then(({ default: VConsole }) => {
+    new VConsole({ theme: "dark" });
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
